04776106b49143847f8109983c3acff73fe17ded
Pēteris Ņikiforovs <p@mak.id.lv>
2014-08-21 16:19:42 +0300

    mono install fail check

--- toolset/setup/linux/languages/mono.sh
+++ toolset/setup/linux/languages/mono.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists mono.installed)
+RETCODE=$(fw_exists $IROOT/mono.fail)
+[ ! "$RETCODE" == 0 ] || { return 1; }
+
+RETCODE=$(fw_exists $IROOT/mono.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
 sudo apt-get install -y \
@@ -15,13 +18,13 @@ sudo apt-get install -y \
 fw_get http://download.mono-project.com/sources/mono/mono-3.6.0.tar.bz2 -O mono-3.6.0.tar.bz2
 fw_untar mono-3.6.0.tar.bz2
 
-cd mono-3.6.0                                                                     && \
-./configure --prefix=$IROOT/mono-3.6.0-install                                    && \
-make -j4                                                                          && \
-make install                                                                      && \
-                                                                                     \
-echo "Installing RootCAs from Mozilla..."                                         && \
-echo 'y' | sudo $IROOT/mono-3.6.0-install/bin/certmgr -ssl -m https://nuget.org   && \
-sudo $IROOT/mono-3.6.0-install/bin/mozroots --import --sync --machine             && \
-                                                                                     \
-touch $IROOT/mono.installed
+(cd mono-3.6.0                                                                              && \
+./configure --prefix=$IROOT/mono-3.6.0-install                                              && \
+make -j4                                                                                    && \
+make install                                                                                && \
+                                                                                               \
+echo "Installing RootCAs from Mozilla..."                                                   && \
+(echo 'y' | sudo $IROOT/mono-3.6.0-install/bin/certmgr -ssl -m https://nuget.org || true)   && \
+sudo $IROOT/mono-3.6.0-install/bin/mozroots --import --sync --machine                       && \
+                                                                                               \
+touch $IROOT/mono.installed) || touch $IROOT/mono.fail
