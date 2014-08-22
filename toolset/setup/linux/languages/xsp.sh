d107c617941c74e5e369d7f13356058d331cab45
Pēteris Ņikiforovs <p@mak.id.lv>
2014-08-22 16:36:57 +0300

    restore xsp from git

--- toolset/setup/linux/languages/xsp.sh
+++ toolset/setup/linux/languages/xsp.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+RETCODE=$(fw_exists xsp.installed)
+[ ! "$RETCODE" == 0 ] || { return 0; }
+
+fw_depends mono
+git clone git://github.com/mono/xsp
+cd xsp
+git checkout 8a31bc625727594d42f94173768bee5cf8afd0a4
+
+./autogen.sh --prefix=/usr
+make
+make install
+
+touch $IROOT/xsp.installed
